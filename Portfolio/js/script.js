a6aef132b2e4f813033522b6870b71cae62426a4
Ray <153441206+VenomRay@users.noreply.github.com>
2024-02-29 09:37:33 -0500

    Add files via upload

---
+++ Portfolio/js/script.js
@@ -0,0 +1,86 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+// circle mouse 
+const circleElement = document.querySelector('.circle');
+
+const mouse = { x: 0, y: 0 };
+const previousMouse = { x: 0, y: 0 };
+const circle = { x: 0, y: 0 };
+let currentScale = 0;
+
+window.addEventListener('mousemove', (e) => {
+    mouse.x = e.x;
+    mouse.y = e.y;
+});
+
+const speed = 0.17;
+
+const tick = () => {
+    circle.x += (mouse.x - circle.x) * speed;
+    circle.y += (mouse.y - circle.y) * speed;
+
+    const deltaMouseX = mouse.x - previousMouse.x;
+    const deltaMouseY = mouse.y - previousMouse.y;
+    previousMouse.x = mouse.x;
+    previousMouse.y = mouse.y;
+
+    const mouseVelocity = Math.min(Math.sqrt(deltaMouseX ** 2 + deltaMouseY ** 2)* 4, 150);
+    const scaleValue = (mouseVelocity / 150) * 0.5;
+    currentScale += (scaleValue - currentScale) * speed;
+    const scaleTransform = `scale(${1 + currentScale}, ${1 - currentScale})`;
+
+    const angle = Math.atan2(deltaMouseY, deltaMouseX) * 180 / Math.PI;
+    const rotateTransform = `rotate(${angle}deg)`;
+
+    circleElement.style.transform = `translate(${circle.x}px, ${circle.y}px) ${scaleTransform} ${rotateTransform}`;
+
+    window.requestAnimationFrame(tick);
+};
+
+tick();
+
+
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+// scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;   
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+
+            navLinks.forEach(links => {
+
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+
+            });
+        }
+    });
+
+  
+
+    // sticky header
+     let header = document.querySelector("header");
+
+     header.classList.toggle("sticky",window.scrollY > 100);
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+    // animation footer on scroll
+
+}
